79a5aa563c77908fdc6b11c98d59b6cbc8341579
ArthurMendes <153393682+Arthurmendescouto@users.noreply.github.com>
2024-06-30 22:42:05 -0300

    atualizar

--- index.js
+++ index.js
@@ -3,9 +3,21 @@ const app=express()
 const handlebars=require('express-handlebars')
 const engine=handlebars.engine
 
-
+//config
+//template engine
 app.engine('handlebars',engine({defaultLayout:'main'}))
 app.set('view engine','handlebars')
+//conexão
+const Sequelize=require('sequelize')
+const sequelize= new Sequelize('test','root','root',{
+    host:'localhost',
+    dialect:'mysql'
+})
+//Rotas
+app.get('/cad',function(req, res){
+    res.send('ROTA DE CADASTRO DE POSTS')
+})
+
 app.listen(8081,function(){
     console.log('Servidor Rodando na url http:localhost:8081')
 })
