368f9825696ab41414c243c54825bcfb5a835e7e
ArthurMendes <153393682+Arthurmendescouto@users.noreply.github.com>
2024-07-01 14:56:46 -0300

    atualizar

--- index.js
+++ index.js
@@ -2,20 +2,29 @@ const express=require('express')
 const app=express()
 const handlebars=require('express-handlebars')
 const engine=handlebars.engine
+const bodyParser=require('body-parser')
+const Sequelize=require('sequelize')
 
 //config
 //template engine
 app.engine('handlebars',engine({defaultLayout:'main'}))
 app.set('view engine','handlebars')
+
+//bodyparser
+app.use(bodyParser.urlencoded({extended:false}))
 //conexão
-const Sequelize=require('sequelize')
+
 const sequelize= new Sequelize('test','root','root',{
     host:'localhost',
     dialect:'mysql'
 })
 //Rotas
 app.get('/cad',function(req, res){
-    res.send('ROTA DE CADASTRO DE POSTS')
+    res.render('formulario')
+})
+
+app.post('/add',function(req,res){
+    res.send("Texto: "+req.body.titulo+" Conteudo: "+req.body.conteudo)
 })
 
 app.listen(8081,function(){
